e416fcbe39c441043b55f25ae26bd8ab9473aa6a
udaihaiti <udaihaiti@gmail.com>
2024-12-08 16:56:00 -0500

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -40,7 +40,7 @@ exports.login = async (req, res) => {
                 institutionId: institution.id,
                 acronym: institution.acronym,
                 status: institution.status,
-                name: institution.name
+                name: institution.nomext
             },
             privateKey,
             { expiresIn: '7d' }
